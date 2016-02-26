3afe3fd9d347e0a5548a2736a40b7f1dfc201f44
mstabile75 <mstabile75@gmail.com>
2016-02-26 12:46:23 -0500

    .

--- js/navigation.js
+++ js/navigation.js
@@ -1,16 +1,13 @@
-$('#kl-nav').load("/navigation.html");
-
-/* Set the active link in the navigation by reading the url path and
-   and comparing it to the links in navigation */
-var url = $.url();
-$(document).ready(function(){
+// load the navigation bar
+$('#kl-nav').load("/navigation.html", function(){
+	/* Set the active link in the navigation by reading the url path and
+   	   and comparing it to the links in navigation */
+   	var url = $.url();
 	$(".navbar").find('a').each( function () {
-		alert("ran nav")
 		if (url.attr('path') == $(this).attr('href')) {
 			$(this).closest( "li" ).addClass("active")
 		} else {
 			$(this).closest( "li" ).removeClass("active")
 		}
 	});
-	
 });
