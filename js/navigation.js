e2949622e0ef30878bd39ab0e9c2c5b0c7fdfe79
mstabile75 <mstabile75@gmail.com>
2016-03-28 11:23:03 -0400

    working

--- js/navigation.js
+++ js/navigation.js
@@ -1,8 +1,8 @@
 // load the navigation bar
-$('#kl-nav').after().load("/navigation.html", function(){
+/*$('#kl-nav').after().load("/navigation.html", function(){
 	/* Set the active link in the navigation by reading the url path and
    	   and comparing it to the links in navigation */
-   	var url = $.url();
+/*   	var url = $.url();
 	$(".navbar").find('a').each( function () {
 		if (url.attr('path') == $(this).attr('href')) {
 			$(this).closest( "li" ).addClass("active")
@@ -10,16 +10,17 @@ $('#kl-nav').after().load("/navigation.html", function(){
 			$(this).closest( "li" ).removeClass("active")
 		}
 	});
-});
+});*/
 
  // Client-side routes    
     Sammy(function() {
         this.get('#:page', function() {
         	var el = $("#kl-page")
             el.empty();
-            el.load("/" + this.params.page + ".html")
-            var offset = el.offset()
-            window.scroll(offset.left, offset.top);
+            el.load("/" + this.params.page + ".html", function(){
+	            var offset = el.offset()
+	            window.scroll(offset.left, offset.top);
+        	});
         });
      	this.get('', function() { 
      		var url = $.url();
